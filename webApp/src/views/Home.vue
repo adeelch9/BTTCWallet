976e09623e64b3cb39242d05dd0ee0e855eefbe7
adeelch9 <adeel.chaudhary@tron.network>
2024-09-12 10:06:22 -0400

    UI Fixes

--- webApp/src/views/Home.vue
+++ webApp/src/views/Home.vue
@@ -116,7 +116,7 @@
                 </ul>
             </div>
 
-            <p class="w600-dots-1" style="direction: ltr;"><span style="font-size: 1rem;" class="w500-dots-1">{{ balance.toFixed(6) }}</span> {{ currencies[currency] }}</p>
+            <p class="w600-dots-1" style="direction: ltr;"><span style="font-size: 1rem;" class="w500-dots-1">{{ balance }}</span> {{ currencies[currency] }}</p>
         </div>
 
         <div id="container-actions">

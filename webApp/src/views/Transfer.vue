b1d8b28a9894cf2d87b155723305bc288cd2896e
adeelch9 <adeel.chaudhary@tron.network>
2024-09-12 12:15:07 -0400

    Update Transfer.vue

--- webApp/src/views/Transfer.vue
+++ webApp/src/views/Transfer.vue
@@ -150,7 +150,7 @@
 
             <div id="container-button">
                 <button :class="['button', 'button-progress', 'normal', `button-progress-${status}`]" @click="Submit"
-                    :disabled="parseFloat(amount) <= 0 || !destination"
+                    :disabled="parseFloat(amount) < 0 || !destination || isNaN(amount)"
                     ><i
                         class="icon-upload"></i><span>{{ $t('transfer.request_transfer') }}</span></button>
             </div>
